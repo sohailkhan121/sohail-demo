a7dae558d4c7e1206904b27ceddf05cc0b737a63
Mohammad Sohail <sohail07277@gmail.com>
2024-11-13 21:21:54 +0530

    adding figlet

---
+++ backend/figlet/script.js
@@ -0,0 +1,10 @@
+var figlet = require("figlet");
+
+figlet("SOHAIL", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
